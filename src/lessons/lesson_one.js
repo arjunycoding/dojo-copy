25e0271827df534844c07b72c04fcd0bad15aa28
Arjun Yuvaraj <arjunycoding@gmail.com>
2023-05-21 20:20:54 -0400

    added

--- src/lessons/lesson_one.js
+++ src/lessons/lesson_one.js
@@ -1,24 +1,34 @@
 import React from "react"
 import LetterBtn from "./letter_button";
 import vowels from "../vowels";
+import { Row, Col, Container } from "react-bootstrap";
 import "../App.css"
 export default function LessonOne() {
     const [current_letter, set_current_letter] = React.useState(false)
     return (
         <div>
             Similar to English Sanskrit also has vowels, and consonants. Click on a vowel to look at it more in detail. Click on the huge letter to here it!
-            <div>
-
-                {vowels.map((letter) => {
-                    return (<button key={letter.letter} onClick={() => { set_current_letter(letter) }}>{letter.letter}</button>)
-                })}
-            </div>
-            <div>
-                {current_letter ?
-                    <LetterBtn symbol={current_letter.letter} audio_file={current_letter.fileName} huge={true} /> :
-                    <p>Please Select A Letter</p>
-                }
-            </div>
+            <br />
+            <br />
+            <Container>
+                <Row>
+                    <Col xs={3}>
+                        {vowels.map((letter) => {
+                            return (<button className="letter_button_select" key={letter.letter} onClick={() => { set_current_letter(letter) }}>{letter.letter}</button>)
+                        })}
+                    </Col>
+                    <Col>
+                        {current_letter ?
+                            <div>
+                                <LetterBtn symbol={current_letter.letter} audio_file={current_letter.fileName} huge={true} />
+                                <p className="none_selected">click to play</p>
+                            </div>
+                            :
+                            <p className="none_selected">-- Please Select A Letter -- </p>
+                        }
+                    </Col>
+                </Row>
+            </Container>
         </div >
     )
 }
--- src/lessons/lesson_two.js
+++ src/lessons/lesson_two.js
@@ -1,24 +1,40 @@
 import React from "react"
 import LetterBtn from "./letter_button";
-import "../App.css"
 import consonants from "../consonants";
+import { Row, Col, Container } from "react-bootstrap";
+import "../App.css"
 export default function LessonOne() {
     const [current_letter, set_current_letter] = React.useState(false)
+    let add_break = 0
     return (
         <div>
             Similar to English Sanskrit also has vowels, and consonants. Click on a consonant to look at it more in detail. Click on the huge letter to here it! However, don't be overwhelmed by the amount of consonants. Don't worry! With constant practice and dedication you will learn them in no time!
-            <div>
-
-                {consonants.map((letter) => {
-                    return (<button key={letter.letter} onClick={() => { set_current_letter(letter) }}>{letter.letter}</button>)
-                })}
-            </div>
-            <div>
-                {current_letter ?
-                    <LetterBtn symbol={current_letter.letter} audio_file={current_letter.fileName} huge={true} /> :
-                    <p>Please Select A Letter</p>
-                }
-            </div>
+            <br />
+            <br />
+            <Container>
+                <Row>
+                    <Col sm={3}>
+                        {consonants.map((letter) => {
+                            add_break++
+                            return (<span>
+                                <button className="letter_button_select" key={letter.letter} onClick={() => { set_current_letter(letter) }}>{letter.letter}</button>
+                                {console.log(add_break % 5)}
+                                {(add_break % 5 == 0 && add_break < 29) || add_break == 29 || add_break === 33 ? <br /> : ""}
+                            </span>)
+                        })}
+                    </Col>
+                    <Col>
+                        {current_letter ?
+                            <div>
+                                <LetterBtn symbol={current_letter.letter} audio_file={current_letter.fileName} huge={true} />
+                                <p className="none_selected">click to play</p>
+                            </div>
+                            :
+                            <p className="none_selected">-- Please Select A Letter -- </p>
+                        }
+                    </Col>
+                </Row>
+            </Container>
         </div >
     )
 }
