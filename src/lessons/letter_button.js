ec2ca7063c1d6622e61bfc2e3f38c29edbbb791b
Arjun Yuvaraj <arjunycoding@gmail.com>
2023-05-21 20:29:32 -0400

    added

--- src/lessons/letter_button.js
+++ src/lessons/letter_button.js
@@ -1,6 +1,6 @@
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
 import '../App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 export default function LetterBtn(props) {
     function play_audio() {
         new Audio(props.audio_file).play()
@@ -15,7 +15,8 @@ export default function LetterBtn(props) {
     } else if (props.audio_btn) {
         return (<button className="hidden_letter_button" onMouseOver={() => { play_audio() }} onClick={() => { props.on_click() }}></button>)
     } else if (props.huge) {
-        <button className="letter_button_huge" onClick={() => { play_audio() }}>{props.symbol}</button>
+        return (<button className="letter_button_huge" onClick={() => { play_audio() }}>{props.symbol}</button>)
+
     }
     return (
         <button className="letter_button" onClick={() => { play_audio() }}>{props.symbol}</button>
